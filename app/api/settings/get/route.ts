f4b490f7910337d6f83ed3a49185215071a11037
AnshulGarg2004 <ganshul2004@gmail.com>
2026-02-06 19:39:08 +0530

    fix : fixed the mongoDb connection

--- app/api/settings/get/route.ts
+++ app/api/settings/get/route.ts
@@ -5,12 +5,18 @@ import { NextRequest, NextResponse } from "next/server";
 export const POST = async (req: NextRequest) => {
     try {
         const { ownerId } = await req.json();
+        console.log("Owner id: in api/s/g ", ownerId);
+        
         if (!ownerId) {
             return NextResponse.json({ success: false, message: "Owner does not exist", ownerId }, { status: 400 });
         }
 
         await connectDB();
+        console.log("mongo ok now fetching sett from db");
+        
         const settings = await Settings.findOne({ ownerId });
+        console.log(settings);
+        
         return NextResponse.json({ success: true, message: "User created or updated", settings }, { status: 200 })
     } catch (error) {
         return NextResponse.json({ success: false, message: `Settings error: ${error}` }, { status: 500 });
--- lib/connectDB.ts
+++ lib/connectDB.ts
@@ -2,32 +2,32 @@ import mongoose from "mongoose"
 
 let cache = global.mongoose;
 
-if(!cache) {
-    global.mongoose = {
+if (!cache) {
+    cache = global.mongoose = {
         conn: null,
         promise: null
     }
 }
 
+const connectDB = async () => {
 
-
-const connectDB = async() => {
-
-    if(cache.conn) {
+    if (cache.conn) {
         return cache.conn;
     }
-    if(!cache.promise) {
-        cache.promise = mongoose.connect(`${process.env.MONOGODB_URI!}`).then((c) => c.connection);
 
+    if (!cache.promise) {
+        cache.promise = mongoose
+            .connect(`${process.env.MONOGODB_URI!}`)
+            .then((c) => c.connection);
     }
+
     try {
         cache.conn = await cache.promise;
         console.log("Mongodb Connected Successfully");
-        
-
+        return cache.conn;
     } catch (error) {
         console.log("Error in connecting to db: ", error);
-        process.exit(1);
+        throw error; // ❗ important for serverless instead of process.exit
     }
 }
 
